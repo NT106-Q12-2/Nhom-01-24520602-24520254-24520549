32e0114747a9dac17422716820912916069504a4
KhanhDang332006 <dangnguyen332006@gmail.com>
2025-11-17 18:58:08 +1400

    Fix giao diện lab4/ bài5

--- Lab04/Bai5/Form1.Designer.cs
+++ Lab04/Bai5/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Bai5
+﻿namespace Bai4
 {
     partial class Form1
     {
@@ -28,13 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.lbl_URL = new System.Windows.Forms.Label();
+            this.tb_Username = new System.Windows.Forms.TextBox();
+            this.tb_URL = new System.Windows.Forms.TextBox();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.rtb_Message = new System.Windows.Forms.RichTextBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.Location = new System.Drawing.Point(38, 79);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(105, 43);
+            this.lbl_Username.TabIndex = 0;
+            this.lbl_Username.Text = "Username:";
+            this.lbl_Username.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.Location = new System.Drawing.Point(38, 122);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(105, 43);
+            this.lbl_Password.TabIndex = 1;
+            this.lbl_Password.Text = "Password:";
+            this.lbl_Password.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_URL
+            // 
+            this.lbl_URL.Location = new System.Drawing.Point(38, 36);
+            this.lbl_URL.Name = "lbl_URL";
+            this.lbl_URL.Size = new System.Drawing.Size(105, 43);
+            this.lbl_URL.TabIndex = 2;
+            this.lbl_URL.Text = "URL:";
+            this.lbl_URL.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tb_Username
+            // 
+            this.tb_Username.Location = new System.Drawing.Point(140, 79);
+            this.tb_Username.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tb_Username.Name = "tb_Username";
+            this.tb_Username.Size = new System.Drawing.Size(358, 30);
+            this.tb_Username.TabIndex = 3;
+            // 
+            // tb_URL
+            // 
+            this.tb_URL.Location = new System.Drawing.Point(140, 36);
+            this.tb_URL.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tb_URL.Name = "tb_URL";
+            this.tb_URL.Size = new System.Drawing.Size(533, 30);
+            this.tb_URL.TabIndex = 3;
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.Location = new System.Drawing.Point(140, 122);
+            this.tb_Password.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.Size = new System.Drawing.Size(358, 30);
+            this.tb_Password.TabIndex = 3;
+            // 
+            // rtb_Message
+            // 
+            this.rtb_Message.Location = new System.Drawing.Point(42, 169);
+            this.rtb_Message.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rtb_Message.Name = "rtb_Message";
+            this.rtb_Message.Size = new System.Drawing.Size(631, 317);
+            this.rtb_Message.TabIndex = 4;
+            this.rtb_Message.Text = "";
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(559, 80);
+            this.btn_Login.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(114, 72);
+            this.btn_Login.TabIndex = 5;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(737, 518);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.rtb_Message);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.tb_URL);
+            this.Controls.Add(this.tb_Username);
+            this.Controls.Add(this.lbl_URL);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Font = new System.Drawing.Font("Times New Roman", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Bai5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Label lbl_URL;
+        private System.Windows.Forms.TextBox tb_Username;
+        private System.Windows.Forms.TextBox tb_URL;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.RichTextBox rtb_Message;
+        private System.Windows.Forms.Button btn_Login;
     }
 }
 
--- Lab04/Bai5/Form1.cs
+++ Lab04/Bai5/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Bai5
+namespace Bai4
 {
     public partial class Form1 : Form
     {
