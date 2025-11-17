d7e741ad93f8c8145bd5bb2df9d61d1a1f402a94
HugndUIT <hungnd1112006@gmail.com>
2025-11-17 13:35:21 +0700

    Hoàn thành Lab 4 - Bài 6: Hoàn thành cả bài

---
+++ Lab04/Bai6/Form_Bai_06.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Bai6
+{
+    partial class Form_Bai_06
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.rtb_Message = new System.Windows.Forms.RichTextBox();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.tb_URL = new System.Windows.Forms.TextBox();
+            this.tb_Username = new System.Windows.Forms.TextBox();
+            this.lbl_URL = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(480, 56);
+            this.btn_Login.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(169, 65);
+            this.btn_Login.TabIndex = 13;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click_1);
+            // 
+            // rtb_Message
+            // 
+            this.rtb_Message.Location = new System.Drawing.Point(18, 146);
+            this.rtb_Message.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rtb_Message.Name = "rtb_Message";
+            this.rtb_Message.Size = new System.Drawing.Size(631, 317);
+            this.rtb_Message.TabIndex = 12;
+            this.rtb_Message.Text = "";
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.Location = new System.Drawing.Point(116, 99);
+            this.tb_Password.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.Size = new System.Drawing.Size(358, 22);
+            this.tb_Password.TabIndex = 9;
+            // 
+            // tb_URL
+            // 
+            this.tb_URL.Location = new System.Drawing.Point(116, 13);
+            this.tb_URL.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tb_URL.Name = "tb_URL";
+            this.tb_URL.Size = new System.Drawing.Size(533, 22);
+            this.tb_URL.TabIndex = 10;
+            // 
+            // tb_Username
+            // 
+            this.tb_Username.Location = new System.Drawing.Point(116, 56);
+            this.tb_Username.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tb_Username.Name = "tb_Username";
+            this.tb_Username.Size = new System.Drawing.Size(358, 22);
+            this.tb_Username.TabIndex = 11;
+            // 
+            // lbl_URL
+            // 
+            this.lbl_URL.Location = new System.Drawing.Point(15, 3);
+            this.lbl_URL.Name = "lbl_URL";
+            this.lbl_URL.Size = new System.Drawing.Size(105, 43);
+            this.lbl_URL.TabIndex = 8;
+            this.lbl_URL.Text = "URL:";
+            this.lbl_URL.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.Location = new System.Drawing.Point(15, 89);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(105, 43);
+            this.lbl_Password.TabIndex = 7;
+            this.lbl_Password.Text = "Password:";
+            this.lbl_Password.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.Location = new System.Drawing.Point(15, 46);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(105, 43);
+            this.lbl_Username.TabIndex = 6;
+            this.lbl_Username.Text = "Username:";
+            this.lbl_Username.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form_Bai_06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 477);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.rtb_Message);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.tb_URL);
+            this.Controls.Add(this.tb_Username);
+            this.Controls.Add(this.lbl_URL);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Name = "Form_Bai_06";
+            this.Text = "Bài 06";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.RichTextBox rtb_Message;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.TextBox tb_URL;
+        private System.Windows.Forms.TextBox tb_Username;
+        private System.Windows.Forms.Label lbl_URL;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Label lbl_Username;
+    }
+}
+
---
+++ Lab04/Bai6/Form_Bai_06.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace Bai6
+{
+    public partial class Form_Bai_06 : Form
+    {
+        public Form_Bai_06()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_Login_Click_1(object sender, EventArgs e)
+        {
+            string url = "https://nt106.uitiot.vn/auth/token";
+            string user_name = tb_Username.Text.Trim();
+            string password = tb_Password.Text.Trim();
+
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new MultipartFormDataContent
+                {
+                    { new StringContent (user_name), "username" },
+                    { new StringContent (password), "password" }
+                };
+
+                var response = await client.PostAsync(url, content);
+
+                var response_string = await response.Content.ReadAsStringAsync();
+
+                rtb_Message.Text = "";
+
+                var response_object = JObject.Parse(response_string);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    var detail = response_object["detail"].ToString();
+                    rtb_Message.Text = $"Lỗi đăng nhập: {detail}";
+                    return;
+                }
+
+                var accessToken = response_object["access_token"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+                var Get_User_Url = tb_URL.Text.Trim();
+                var Get_User_Response = await client.GetAsync(Get_User_Url);
+                var Get_User_Response_String = await Get_User_Response.Content.ReadAsStringAsync();
+                var JSON_String = JObject.Parse(Get_User_Response_String);
+
+                rtb_Message.Text += "Username: " + JSON_String["username"] + "\n";
+                rtb_Message.Text += "Email: " + JSON_String["email"] + "\n";
+                rtb_Message.Text += "Password: " + JSON_String["password"] + "\n";
+                rtb_Message.Text += "First name: " + JSON_String["first_name"] + "\n";
+                rtb_Message.Text += "Last name: " + JSON_String["last_name"] + "\n";
+                rtb_Message.Text += "Sex: " + JSON_String["sex"] + "\n";
+                rtb_Message.Text += "Birthday: " + JSON_String["birthday"] + "\n";
+                rtb_Message.Text += "Language: " + JSON_String["language"] + "\n";
+                rtb_Message.Text += "Phone: " + JSON_String["phone"] + "\n";
+            }
+        }
+    }
+}
