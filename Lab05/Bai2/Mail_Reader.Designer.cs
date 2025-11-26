bf100b47370f52aae95cc689377702e665d65072
hoangnxm <hoangminh13022006@gmail.com>
2025-11-26 15:13:04 +0700

    Hoàn thành bài 2

---
+++ Lab05/Bai2/Mail_Reader.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Bai2
+{
+    partial class Mail_Reader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lv_Info = new System.Windows.Forms.ListView();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.tb_Email = new System.Windows.Forms.TextBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.lbl_Recent = new System.Windows.Forms.Label();
+            this.lbl_Total = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lv_Info
+            // 
+            this.lv_Info.HideSelection = false;
+            this.lv_Info.Location = new System.Drawing.Point(12, 154);
+            this.lv_Info.Name = "lv_Info";
+            this.lv_Info.Size = new System.Drawing.Size(776, 279);
+            this.lv_Info.TabIndex = 19;
+            this.lv_Info.UseCompatibleStateImageBehavior = false;
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Password.Location = new System.Drawing.Point(125, 61);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.Size = new System.Drawing.Size(191, 23);
+            this.tb_Password.TabIndex = 18;
+            // 
+            // tb_Email
+            // 
+            this.tb_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Email.Location = new System.Drawing.Point(125, 17);
+            this.tb_Email.Name = "tb_Email";
+            this.tb_Email.Size = new System.Drawing.Size(191, 23);
+            this.tb_Email.TabIndex = 17;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.Location = new System.Drawing.Point(619, 47);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(169, 34);
+            this.btn_Login.TabIndex = 16;
+            this.btn_Login.Text = "LOGIN";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // lbl_Recent
+            // 
+            this.lbl_Recent.AutoSize = true;
+            this.lbl_Recent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Recent.Location = new System.Drawing.Point(318, 112);
+            this.lbl_Recent.Name = "lbl_Recent";
+            this.lbl_Recent.Size = new System.Drawing.Size(0, 17);
+            this.lbl_Recent.TabIndex = 15;
+            // 
+            // lbl_Total
+            // 
+            this.lbl_Total.AutoSize = true;
+            this.lbl_Total.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Total.Location = new System.Drawing.Point(148, 112);
+            this.lbl_Total.Name = "lbl_Total";
+            this.lbl_Total.Size = new System.Drawing.Size(0, 17);
+            this.lbl_Total.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(225, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Recent:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Total:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Email:";
+            // 
+            // Mail_Reader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lv_Info);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.tb_Email);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.lbl_Recent);
+            this.Controls.Add(this.lbl_Total);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Mail_Reader";
+            this.Text = "Mail_Reader_IMAP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv_Info;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.TextBox tb_Email;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label lbl_Recent;
+        private System.Windows.Forms.Label lbl_Total;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab05/Bai2/Mail_Reader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using MailKit.Net.Imap;
+using MailKit;
+using MimeKit;
+
+
+namespace Bai2
+{
+    public partial class Mail_Reader : Form
+    {
+        public Mail_Reader()
+        {
+            InitializeComponent();
+            LoadListView();
+        }
+
+        private void LoadListView()
+        {
+            lv_Info.View = View.Details;
+            lv_Info.Items.Clear();
+            lv_Info.Columns.Clear();
+            lv_Info.Columns.Add("Email", 300);
+            lv_Info.Columns.Add("From", 300);
+            lv_Info.Columns.Add("Thời gian", 200);
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_Email.Text) || string.IsNullOrEmpty(tb_Password.Text)) 
+            {
+                MessageBox.Show("Không bỏ trống thông tin nào", "Lỗi");
+                return;
+            }
+
+            lbl_Recent.Text = "";
+            lbl_Total.Text = "";
+            using (var client = new ImapClient())
+            {
+                client.Connect("imap.gmail.com", 993, true);
+                client.Authenticate(tb_Email.Text, tb_Password.Text);
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
+
+                for (int i = 0; i < inbox.Count; i++)
+                {
+                    var message = inbox.GetMessage(i);
+                    AddInfo(message);
+                }
+                lbl_Recent.Text = inbox.Recent.ToString();
+                lbl_Total.Text = inbox.Count.ToString();
+            }
+        }
+
+        private void AddInfo(MimeMessage message)
+        {
+            ListViewItem Item = new ListViewItem(message.Subject.ToString());
+            Item.SubItems.Add(message.From.ToString());
+            Item.SubItems.Add(message.Date.ToString());
+            lv_Info.Items.Add(Item);
+        }
+    }
+}
+//app password:pews jhiz anla tsjy
