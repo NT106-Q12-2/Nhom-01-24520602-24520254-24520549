038401d6b682bd79df891e945d542416329f09d8
24520602 <24520602@gm.uit.edu.vn>
2025-11-26 15:21:57 +0700

    Hoàn thành Lab 5 bài 3

---
+++ Lab05/Bai3/Mail_Reader_Pop.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Bai3
+{
+    partial class Mail_Reader_Pop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.lbl_total = new System.Windows.Forms.Label();
+            this.lv_danhsachmail = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // tb_email
+            // 
+            this.tb_email.Location = new System.Drawing.Point(124, 3);
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(457, 22);
+            this.tb_email.TabIndex = 2;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(124, 40);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '*';
+            this.tb_password.Size = new System.Drawing.Size(457, 22);
+            this.tb_password.TabIndex = 3;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(607, 3);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(181, 58);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "LOGIN";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // lbl_total
+            // 
+            this.lbl_total.AutoSize = true;
+            this.lbl_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_total.Location = new System.Drawing.Point(0, 74);
+            this.lbl_total.Name = "lbl_total";
+            this.lbl_total.Size = new System.Drawing.Size(68, 25);
+            this.lbl_total.TabIndex = 5;
+            this.lbl_total.Text = "Total:";
+            // 
+            // lv_danhsachmail
+            // 
+            this.lv_danhsachmail.HideSelection = false;
+            this.lv_danhsachmail.Location = new System.Drawing.Point(6, 105);
+            this.lv_danhsachmail.Name = "lv_danhsachmail";
+            this.lv_danhsachmail.Size = new System.Drawing.Size(782, 425);
+            this.lv_danhsachmail.TabIndex = 7;
+            this.lv_danhsachmail.UseCompatibleStateImageBehavior = false;
+            // 
+            // Mail_Reader_Pop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 542);
+            this.Controls.Add(this.lv_danhsachmail);
+            this.Controls.Add(this.lbl_total);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_email);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Mail_Reader_Pop";
+            this.Text = "Mail Reader Pop3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_email;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label lbl_total;
+        private System.Windows.Forms.ListView lv_danhsachmail;
+    }
+}
+
---
+++ Lab05/Bai3/Mail_Reader_Pop.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+using MailKit.Net.Pop3;
+
+namespace Bai3
+{
+    public partial class Mail_Reader_Pop : Form
+    {
+        public Mail_Reader_Pop()
+        {
+            InitializeComponent();
+            lv_danhsachmail.View = View.Details;    
+            lv_danhsachmail.Items.Clear();  
+            lv_danhsachmail.Columns.Clear();
+            lv_danhsachmail.Columns.Add("Email", 200);
+            lv_danhsachmail.Columns.Add("From", 200);
+            lv_danhsachmail.Columns.Add("Time", 200);
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_email.Text) || string.IsNullOrEmpty(tb_password.Text)) {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!");
+                return;
+            }
+
+            try
+            {
+                using (var Client = new Pop3Client())
+                {
+                    Client.Connect("pop.gmail.com", 995, true);
+                    Client.Authenticate(tb_email.Text.Trim(), tb_password.Text.Trim());
+
+                    var Count_Mail = Client.Count;
+
+                    lbl_total.Text = "Total: ";
+                    lbl_total.Text += Count_Mail.ToString();
+
+                    for (int i = 0; i < Count_Mail; i++)
+                    {
+                        var Message = Client.GetMessage(i);
+                        ListViewItem Item = new ListViewItem(Message.Subject.ToString());
+                        Item.SubItems.Add(Message.From.ToString());
+                        Item.SubItems.Add(Message.Date.ToString());
+                        lv_danhsachmail.Items.Add(Item);
+                    }
+                }
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
+// App password: rigu cwqp sjrd bkmk 
