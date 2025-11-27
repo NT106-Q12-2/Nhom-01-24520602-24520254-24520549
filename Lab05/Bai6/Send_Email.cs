7a33f26414081d251719968d15b41b9b6090ec4b
24520602 <24520602@gm.uit.edu.vn>
2025-11-27 13:49:26 +0700

    Hoàn thành chức năng gửi mail

--- Lab05/Bai6/Send_Email.cs
+++ Lab05/Bai6/Send_Email.cs
@@ -7,20 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
 
 namespace Bai6
 {
     public partial class frm_SendEmail: Form
     {
-        public frm_SendEmail(string email)
+        int Port;
+        string Host;
+        string Email;
+        string Password;
+
+        public frm_SendEmail(string Email, string Password, string Host, int Port)
         {
             InitializeComponent();
-            tb_EmailSender.Text = email;
+            this.Host = Host;
+            this.Port = Port;
+            this.Email = Email;
+            this.Password = Password;
         }
 
-        private void frm_SendEmail_Load(object sender, EventArgs e)
+        private void btn_Browse_Click(object sender, EventArgs e)
         {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tb_Attach.Text = ofd.FileName;
+            }
+        }
 
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_EmailSender.Text)
+                || string.IsNullOrEmpty(tb_Subject.Text)
+                || string.IsNullOrEmpty(rtb_Body.Text))
+            MessageBox.Show("Vui lòng nhập đi đủ thông tin!");
+
+            try
+            {
+                using (var Client = new SmtpClient())
+                {
+                    Client.Connect(Host, Port, true);
+                    Client.Authenticate(Email, Password);
+
+                    var Message = new MimeMessage();
+                    Message.From.Add(new MailboxAddress(tb_Name.Text, tb_EmailSender.Text));
+                    Message.To.Add(new MailboxAddress("", tb_EmailReceived.Text));
+                    Message.Subject = tb_Subject.Text;
+                    var builder = new BodyBuilder();
+
+                    if (cb_HTML.Checked)
+                    {
+                        builder.HtmlBody = rtb_Body.Text;
+                    }
+                    else
+                    {
+                        builder.TextBody = rtb_Body.Text;
+                    }
+
+                    string File_Path = tb_Attach.Text;
+                    if (!string.IsNullOrEmpty(File_Path) && System.IO.File.Exists(File_Path))
+                    {
+                        builder.Attachments.Add(File_Path);
+                    }
+
+                    Message.Body = builder.ToMessageBody();
+
+                    Client.Send(Message);
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Error");
+            }
         }
     }
 }
