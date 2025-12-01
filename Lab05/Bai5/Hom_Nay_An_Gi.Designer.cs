42f472def8c67fb30ca6e3d00817d722058e51c9
hoangnxm <hoangminh13022006@gmail.com>
2025-12-01 11:58:35 +0700

    Hoàn thành chức năng tải dữ liệu món ăn xuống database bằng cách đọc mail và hiển thị danh sách món ăn khi tạo món

--- Lab05/Bai5/Hom_Nay_An_Gi.Designer.cs
+++ Lab05/Bai5/Hom_Nay_An_Gi.Designer.cs
@@ -45,9 +45,10 @@ namespace Bai5
             // lv_dsmonan
             // 
             this.lv_dsmonan.HideSelection = false;
-            this.lv_dsmonan.Location = new System.Drawing.Point(14, 54);
+            this.lv_dsmonan.Location = new System.Drawing.Point(10, 44);
+            this.lv_dsmonan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lv_dsmonan.Name = "lv_dsmonan";
-            this.lv_dsmonan.Size = new System.Drawing.Size(301, 375);
+            this.lv_dsmonan.Size = new System.Drawing.Size(227, 305);
             this.lv_dsmonan.TabIndex = 0;
             this.lv_dsmonan.UseCompatibleStateImageBehavior = false;
             // 
@@ -55,9 +56,10 @@ namespace Bai5
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(322, 9);
+            this.label1.Location = new System.Drawing.Point(242, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(248, 41);
+            this.label1.Size = new System.Drawing.Size(202, 32);
             this.label1.TabIndex = 1;
             this.label1.Text = "Hôm Nay Ăn Gì?";
             // 
@@ -65,9 +67,10 @@ namespace Bai5
             // 
             this.btn_themmon.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_themmon.ForeColor = System.Drawing.Color.ForestGreen;
-            this.btn_themmon.Location = new System.Drawing.Point(321, 57);
+            this.btn_themmon.Location = new System.Drawing.Point(241, 46);
+            this.btn_themmon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_themmon.Name = "btn_themmon";
-            this.btn_themmon.Size = new System.Drawing.Size(249, 128);
+            this.btn_themmon.Size = new System.Drawing.Size(187, 104);
             this.btn_themmon.TabIndex = 8;
             this.btn_themmon.Text = "Thêm món ăn";
             this.btn_themmon.UseVisualStyleBackColor = true;
@@ -77,9 +80,10 @@ namespace Bai5
             // 
             this.btn_xemmon.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_xemmon.ForeColor = System.Drawing.Color.Orange;
-            this.btn_xemmon.Location = new System.Drawing.Point(321, 191);
+            this.btn_xemmon.Location = new System.Drawing.Point(241, 155);
+            this.btn_xemmon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_xemmon.Name = "btn_xemmon";
-            this.btn_xemmon.Size = new System.Drawing.Size(249, 119);
+            this.btn_xemmon.Size = new System.Drawing.Size(187, 97);
             this.btn_xemmon.TabIndex = 9;
             this.btn_xemmon.Text = "Xem danh sách";
             this.btn_xemmon.UseVisualStyleBackColor = true;
@@ -89,9 +93,10 @@ namespace Bai5
             // 
             this.btn_chonmon.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_chonmon.ForeColor = System.Drawing.Color.Red;
-            this.btn_chonmon.Location = new System.Drawing.Point(321, 316);
+            this.btn_chonmon.Location = new System.Drawing.Point(241, 257);
+            this.btn_chonmon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_chonmon.Name = "btn_chonmon";
-            this.btn_chonmon.Size = new System.Drawing.Size(249, 110);
+            this.btn_chonmon.Size = new System.Drawing.Size(187, 89);
             this.btn_chonmon.TabIndex = 10;
             this.btn_chonmon.Text = "Chọn món ăn";
             this.btn_chonmon.UseVisualStyleBackColor = true;
@@ -101,10 +106,10 @@ namespace Bai5
             // 
             this.ptb_ketquamonan.BackColor = System.Drawing.Color.White;
             this.ptb_ketquamonan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ptb_ketquamonan.Location = new System.Drawing.Point(576, 57);
-            this.ptb_ketquamonan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ptb_ketquamonan.Location = new System.Drawing.Point(432, 46);
+            this.ptb_ketquamonan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ptb_ketquamonan.Name = "ptb_ketquamonan";
-            this.ptb_ketquamonan.Size = new System.Drawing.Size(323, 369);
+            this.ptb_ketquamonan.Size = new System.Drawing.Size(243, 300);
             this.ptb_ketquamonan.TabIndex = 13;
             this.ptb_ketquamonan.TabStop = false;
             // 
@@ -113,9 +118,10 @@ namespace Bai5
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(49, 18);
+            this.label6.Location = new System.Drawing.Point(37, 15);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(225, 32);
+            this.label6.Size = new System.Drawing.Size(175, 25);
             this.label6.TabIndex = 14;
             this.label6.Text = "Danh sách món ăn";
             // 
@@ -124,18 +130,19 @@ namespace Bai5
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(630, 23);
+            this.label7.Location = new System.Drawing.Point(472, 19);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(211, 32);
+            this.label7.Size = new System.Drawing.Size(166, 25);
             this.label7.TabIndex = 15;
             this.label7.Text = "Hình ảnh món ăn";
             // 
             // Hom_Nay_An_Gi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(914, 438);
+            this.ClientSize = new System.Drawing.Size(686, 356);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.ptb_ketquamonan);
@@ -145,8 +152,10 @@ namespace Bai5
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lv_dsmonan);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Hom_Nay_An_Gi";
             this.Text = "Hôm Nay Ăn Gì";
+            this.Load += new System.EventHandler(this.Hom_Nay_An_Gi_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ptb_ketquamonan)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Lab05/Bai5/Hom_Nay_An_Gi.cs
+++ Lab05/Bai5/Hom_Nay_An_Gi.cs
@@ -8,14 +8,157 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SQLite;
+using MimeKit;
+using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
 
 namespace Bai5
 {
     public partial class Hom_Nay_An_Gi : Form
     {
+        Timer CapNhatMon;
+
+        // Chuỗi kết nối SQL
+        static string ConnectionSQL = "Data Source=SQLHomNayAnGi.db;Version=3;";
         public Hom_Nay_An_Gi()
         {
             InitializeComponent();
+            CapNhatMon = new Timer();
+            CapNhatMon.Interval = 10000;
+            CapNhatMon.Tick += CapNhatMon_Tick;
+        }
+
+        private void Hom_Nay_An_Gi_Load(object sender, EventArgs e)
+        {
+            LoadMonAn();
+
+            CapNhatMon.Start();
+        }
+
+        private async void CapNhatMon_Tick(object sender, EventArgs e)
+        {
+            CapNhatMon.Stop();
+
+            bool IsUpdate = await Task.Run(() => ThemMonDB());
+            if (IsUpdate)
+            {
+                LoadMonAn();
+            }
+            CapNhatMon.Start();
+        }
+
+        private bool ThemMonDB()
+        {
+            bool IsUpdate = false;
+
+            try
+            {
+                using(var client = new ImapClient())
+                {
+                    client.Connect("imap.gmail.com",993);
+                    client.Authenticate("lab5.24520549.24520254.24520602@gmail.com", "opgg amtv ynsy bpdw");
+                    var inbox = client.Inbox;
+                    inbox.Open(FolderAccess.ReadWrite);
+
+                    var query = SearchQuery.NotSeen.And(SearchQuery.SubjectContains("Đóng góp món ăn"));
+                    var mails = inbox.Search(query);
+
+                    foreach(var mail in mails)
+                    {
+                        var message = inbox.GetMessage(mail);
+                        string NguoiGui = message.From.ToString();
+                        string NoiDung = message.TextBody;
+
+                        string[] lines = NoiDung.Split(new [] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (var line in lines)
+                        {
+                            string[] parts = line.Split(';');
+                            if(parts.Length == 2)
+                            {
+                                string TenMon = parts[0].Trim();
+                                string HinhAnh = parts[1].Trim();
+                                if(!string.IsNullOrEmpty(TenMon) && !string.IsNullOrEmpty(HinhAnh))
+                                {
+                                    InsertMonAn(TenMon,HinhAnh,NguoiGui);
+                                    IsUpdate = true;
+                                }
+                            }
+                        }
+                        inbox.AddFlags(mail, MessageFlags.Seen, true);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {}
+            return IsUpdate;
+        }
+
+        private void InsertMonAn(string tenmon, string anh, string nguoiDongGop)
+        {
+            using(SQLiteConnection conn = new SQLiteConnection(ConnectionSQL))
+            {
+                conn.Open();
+                int id = -1;
+
+                string CheckUser = "SELECT IDNCC FROM NguoiDung WHERE HoVaTen = @hoTen";
+                using (SQLiteCommand cmd = new SQLiteCommand(CheckUser, conn))
+                {
+                    cmd.Parameters.AddWithValue("@hoTen", nguoiDongGop);
+                    var result = cmd.ExecuteScalar();
+                    if(result != null)
+                    {
+                        id = Convert.ToInt32(result);
+                    }
+                    else
+                    {
+                        string InsertUser = "INSERT INTO NguoiDung (HoVaTen) VALUES (@hoTen); SELECT last_insert_rowid();";
+                        using(SQLiteCommand cmdInsertUser = new SQLiteCommand(InsertUser, conn))
+                        {
+                            cmdInsertUser.Parameters.AddWithValue("@hoTen", nguoiDongGop);
+                            id = (int)cmdInsertUser.ExecuteScalar();
+                        }
+                    }
+                }
+
+                string queryInsertMon = "INSERT INTO MonAn (TenMonAn,HinhAnh,IDNCC) VALUES (@tenMon, @hinhAnh, @idNCC)";
+                using (SQLiteCommand cmdInsertMon = new SQLiteCommand(queryInsertMon, conn))
+                {
+                    cmdInsertMon.Parameters.AddWithValue("@tenMon", tenmon);
+                    cmdInsertMon.Parameters.AddWithValue("@hinhAnh", anh);
+                    cmdInsertMon.Parameters.AddWithValue("@idNCC", id);
+                    cmdInsertMon.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void LoadMonAn()
+        {
+            lv_dsmonan.Clear();
+
+            using(SQLiteConnection conn = new SQLiteConnection(ConnectionSQL))
+            {
+                conn.Open();
+                string query = @"SELECT m.TenMonAn, m.HinhAnh, n.HoVaTen FROM MonAn m LEFT JOIN NguoiDung n ON m.IDNCC = n.IDNCC";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                {
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string tenMon = reader["TenMonAn"].ToString();
+
+                            string nguoiDongGop = reader["HoVaTen"] != DBNull.Value? reader["HoVaTen"].ToString(): "Người ẩn danh";
+
+                            ListViewItem item = new ListViewItem(tenMon);
+                            item.SubItems.Add(nguoiDongGop);
+  
+                            lv_dsmonan.Items.Add(item);
+                        }
+                    }
+                }
+            }
         }
 
         // Định nghĩa kiểu dữ liệu Food
@@ -25,9 +168,6 @@ namespace Bai5
             public string Image { get; set; }
             public string NameNCC { get; set; }
         }
-
-        // Chuỗi kết nối SQL
-        static string ConnectionSQL = "Data Source=SQLHomNayAnGi.db;Version=3;";
 
         private void btn_themmon_Click(object sender, EventArgs e)
         {
